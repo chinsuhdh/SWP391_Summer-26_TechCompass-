61fc0a9312191be00dc885dda4f7cb6a2b9682e6
Hau1436H <nguyentaihau2@gmail.com>
2026-06-03 20:23:07 +0700

    Delete BE on FE branch

--- SWP391_TechCompass_BE/Service_TechCompass/Interfaces/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Service_TechCompass.Interfaces
-{
-    public interface IEmailService
-    {
-        Task SendEmailAsync(string toEmail, string subject, string body);
-    }
-}
--- SWP391_TechCompass_BE/Service_TechCompass/Interfaces/IQuizSyncService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Service_TechCompass.Interfaces
-{
-    public interface IQuizSyncService
-    {
-        Task<int> FetchAndSaveQuestionsAsync(int skillNodeId, string tags, int limit = 10);
-    }
-}
